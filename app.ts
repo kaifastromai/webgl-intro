183372567c1493b6b4dfeba1b7cd2a61e59c0113
Kaifas Stromai <cephastorm@outlook.com>
2020-03-22 18:04:51 -0500

    init

--- app.ts
+++ app.ts
@@ -1,16 +1,17 @@
 import { drawScene, glcanvas, gl } from "./webgl";
 import { vec4, mat4, vec2, vec3 } from "gl-matrix";
 import { Disc } from "./disc";
-var disc = new Disc(12, 5, 0.001, 5);
-var disc2 = new Disc(12, 5, 2, 3, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 0));
-var disc3 = new Disc(15, 5, 5, 5, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 4));
-var disc4 = new Disc(15, 5, 1, 5, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 4), 270);
-disc.slices = 60;
+import { mesh } from "./mesh";
+import { loadJSON } from "./utils";
+// var disc = new Disc(12, 5, 0.001, 5);
+// var disc2 = new Disc(12, 5, 2, 3, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 0));
+// var disc3 = new Disc(15, 5, 5, 5, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 4));
+// var disc4 = new Disc(15, 5, 1, 5, vec3.fromValues(0, 0, 0), vec3.fromValues(0, 0, 4), 270);
+// disc.slices = 60;
 //disc.regenMesh();
-
 var wireframe: boolean = true;
-disc.inner_radius = 1;
-disc.outer_radius = 4;
+// disc.inner_radius = 1;
+// disc.outer_radius = 4;
 var c_m = 0;
 document.addEventListener('keydown', function (event) {
     if (event.key == 'w') {
@@ -25,12 +26,17 @@ document.addEventListener('keydown', function (event) {
 
 
 async function main() {
-    await disc.initialize();
-    await disc2.initialize();
-    await disc3.initialize();
-    await disc4.initialize();
-    console.log(disc.shader.program);
-    requestAnimationFrame(drawScene);
+    // await disc.initialize();
+    // await disc2.initialize();
+    // await disc3.initialize();
+    // await disc4.initialize();
+    // console.log(disc.shader.program);
+    // requestAnimationFrame(drawScene);
+    let json = await loadJSON("./box.json");
+    let tris = json.meshes.box.vertices;
+    let indices = json.meshes.box.v_index;
+    let mMesh = new mesh(tris, indices);
+    console.log(mMesh);
 }
 main();
 
@@ -74,4 +80,4 @@ function ProjectText(P: vec4, mvp: mat4, ctx: CanvasRenderingContext2D, text: st
     let c = vec2.fromValues((p[0] * 0.5 + 0.5) * gl.canvas.width, (p[1] * -0.5 + 0.5) * gl.canvas.height);
     ctx.fillText(text, c[0], c[1]);
 }
-export { then, disc, wireframe, disc2, disc3, disc4, c_m };
+export { then, /*disc, wireframe, disc2, disc3, disc4, c_m */ };
---
+++ mesh.ts
@@ -0,0 +1,57 @@
+import { vec3, mat4, vec4, mat3 } from "gl-matrix";
+import { vec3FromArray } from "./utils";
+import * as THREE from 'node_modules/three/'
+class mesh {
+    meshFromTris(tris: number[], tri_indices: Array<number>) {
+
+        for (let i = 0; i < tri_indices.length / 3; i++) {
+            this.faces.push(
+                face.fromVec3(vec3FromArray(tris.slice(tri_indices[i], 3 * tri_indices[i] + 3)),
+                    vec3FromArray(tris.slice(3 * tri_indices[i + 1], 3 * tri_indices[i + 1] + 3)),
+                    vec3FromArray(tris.slice(3 * tri_indices[i + 2], 3 * tri_indices[i + 2] + 3))
+                ))
+        }
+
+    }
+
+    /**@param tris An array containing all the vertices of this mesh */
+    /**@param faces an array of all the faces. Containts other mesh data such as vertex*/
+    private faces: Array<face>;
+    /**
+     * 
+     * @param tris the array of vertices that make a triangle
+     */
+    constructor(tris: Array<number>, tri_indices: Array<number>) {
+        this.faces = [];
+        this.meshFromTris(tris, tri_indices);
+    }
+}
+
+class face {
+    /**@param verts */
+    protected verts: Array<vec3>;
+
+    constructor(v1: vec3 = null, v2: vec3 = null, v3: vec3 = null) {
+        this.verts = [];
+        if (v1 == null || v2 == null || v3 == null) {
+
+        }
+        else {
+            this.verts.push(v1, v2, v3);
+        }
+    }
+    /**
+     * Create a face given 3 gl-matrix vec3s;
+     * @param v1 the first vector of the face
+     * @param v2 the second vector of the face
+     * @param v3 the third vector of the face
+     */
+    public static fromVec3(v1: vec3, v2: vec3, v3: vec3): face {
+        let b = new face(v1, v2, v3);
+        return b;
+    }
+
+
+}
+
+export { mesh }
